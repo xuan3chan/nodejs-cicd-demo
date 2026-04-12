abb1d52271df4e1cfaca4d23ac05f77160af1ddf
xuan3chan <xuannguyen04072003@gmail.com>
2026-04-12 17:58:05 +0700

    chore: initialize server project with package configuration and build artifacts

--- server/dist/modules/auth/jwt.strategy.d.ts
+++ server/dist/modules/auth/jwt.strategy.d.ts
@@ -1,6 +1,6 @@
 import { Strategy } from 'passport-jwt';
 import { AuthService } from './auth.service';
-declare const JwtStrategy_base: new (...args: [opt: import("passport-jwt").StrategyOptionsWithRequest] | [opt: import("passport-jwt").StrategyOptionsWithoutRequest]) => Strategy & {
+declare const JwtStrategy_base: new (...args: [opt: import("passport-jwt").StrategyOptionsWithoutRequest] | [opt: import("passport-jwt").StrategyOptionsWithRequest]) => Strategy & {
     validate(...args: any[]): unknown;
 };
 export declare class JwtStrategy extends JwtStrategy_base {
