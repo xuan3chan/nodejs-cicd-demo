a141a3911f2da8ddfbbd1f0f043ebb9b758694d4
xuan3chan <xuannguyen04072003@gmail.com>
2026-04-12 22:36:47 +0700

    feat: add AppService with welcome message method

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getWelcomeMessage(): string {
-    return 'Welcome to Bun Quay 79!';
+    return 'Chào mừng đến với Bún Quậy 79!';
   }
 }
